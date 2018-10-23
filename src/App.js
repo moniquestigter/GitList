ae3949ce3166227a5bac890708afdf8cdd8c67d0
moniquestigter <mstigter17@hotmail.com>
2018-10-23 16:45:46 -0600

    fixed search bar, added browser router; Sem3-martes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header.js'; // Importamos componente Header
 import Search from './Search'; // Importamos componente Search
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <div className="container">
-        <Search />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Search} />
+    </BrowserRouter>
   );
 };
  
--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import Header from './Header.js';
 
 const Search = () => {
   return (
-    <div className="search">
-      <input type="text" placeholder="Enter Github Username">
-      </input>
-      <i className="fa fa-check-circle"></i>
+    <div>
+      <Header />
+      <div className="container">
+        <section className="search six offset-by-three columns">
+          <form>
+            <button type="submit">
+              <span className="fa fa-check-circle fa-3x" />
+            </button>
+            <input
+              className="u-full-width"
+              type="text"
+              name="username"
+              placeholder="Enter Github Username"
+            />
+          </form>
+        </section>
+      </div>
     </div>
-    
   );
 };
 
